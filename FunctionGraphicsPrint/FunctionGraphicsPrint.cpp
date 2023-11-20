334974d701f6a7cd833cd0b23706a06705ef59ef
Ян Вевюр <shadowfantomch@outlook.com>
2023-11-20 23:42:42 +0500

    Done some cool draw features

--- FunctionGraphicsPrint/FunctionGraphicsPrint.cpp
+++ FunctionGraphicsPrint/FunctionGraphicsPrint.cpp
@@ -5,32 +5,73 @@
 #include <iomanip>
 using namespace std;
 
-int f(int x)
+int f2(int x)
 {
+    return 0;
+    //return 
     //return 10*sin(2 * sin(2 * sin(2 * sin(x * 0.1))));
     //return 10*sin(x*0.1);
+    //return 10*cos(x*0.1);
     //return 1 / tan(x);
     //return 5*tan(x*0.1);
-    //return log(x);
+    //return log(x*2);
     //return x * x * x;
     //return x*x;
     //return x;
-    //return sqrt(x);
+    //return sqrt(x*2);
     //return -sqrt(x);
-    //if (x == 0) { return 0; } return 15/x;
+    //if (x == 0) { return NULL; } return 15 / x;
+}
+
+int f1(int x)
+{
+    return 0;
+    //return 
+    //return 10*sin(2 * sin(2 * sin(2 * sin(x * 0.1))));
+    //return 10*sin(x*0.1);
+    //return 10*cos(x*0.1);
+    //return 1 / tan(x);
+    //return 5*tan(x*0.1);
+    //return log(x*2);
+    //return x * x * x;
+    //return x*x;
+    //return x;
+    //return sqrt(x*2);
+    //return -sqrt(x);
+    //if (x == 0 ) { return x; } return 15/x;
 }
 
 
-void drawFunction(char** arr, int h, int w)
+void drawFunction(char** arr, int h, int w, int(f)(int))
 {
-
     for (int i = 0; i < h; i++)
     {
         for (int j = 0; j < w; j++)
         {
-            if (f((j-w/2)) == (h / 2 - i))
+            if (f(j-w/2) == (h / 2 - i))
             {
-                arr[i][j] =  '*';
+                if (f(j - w / 2) > f(j + 1 - w / 2))
+                {
+                    if ((f(j - w / 2) == 0 || (j - w / 2) == 0) && f(j - 1 - w / 2) > f(j + 1 - w / 2))
+                    {
+                        arr[i][j] = '+';
+                    }
+                    else arr[i][j] = '\\';
+                }
+                else if (f(j - w / 2) < f(j + 1 - w / 2))
+                {
+                    if ((f(j - w / 2) == 0 || (j - w / 2) == 0) && f(j - 1 - w / 2) < f(j + 1 - w / 2))
+                    {
+                        arr[i][j] = '+';
+                    }
+                    else arr[i][j] = '/';
+
+                }
+                else if (f(j - w / 2) == 0 && (h / 2 - i) == 0)
+                {
+                    arr[i][j] = '=';
+                }
+                else arr[i][j] =  '*';
             }
         }
     }
@@ -87,16 +128,20 @@ void printArr(char** arr, int h, int w)
 
 int main()
 {
-    int w = 151;
-    int h = 41; 
+    int w = 141;
+    int h = 141; 
     char** plane = new char*[h];
     for (int i = 0; i < h; i++)
     {
         plane[i] = new char[w];
     }
     clearPlane(plane, h, w);
-    drawFunction(plane, h, w);
     drawAxis(plane, h, w);
+    drawFunction(plane, h, w, f1);
+    drawFunction(plane, h, w, f2);
+
+    
+   
     printArr(plane, h, w);
 
     delete[] plane;
